f5461d377a7df5e4ebca052ea31fb229982ab5c9
miyu.s <s185015@dezainjouhougakka-no-MacBook-Pro-2.local>
2017-09-02 23:46:24 +0900

    Initial Commit

---
+++ しゅくまる/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  しゅくまる
+//
+//  Created by miyu.s on 2017/09/02.
+//  Copyright © 2017年 miyu.s. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ しゅくまる/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  しゅくまる
+//
+//  Created by miyu.s on 2017/09/02.
+//  Copyright © 2017年 miyu.s. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
