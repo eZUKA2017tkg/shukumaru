f529130bb1e05cc7582cd0ec234ff7fd891a1a52
miyu.s <s185015@dezainjouhougakka-no-MacBook-Pro-2.local>
2017-11-04 22:14:07 +0900

    10/4 しゅくだいリスト

--- しゅくまる/tableViewList.swift
+++ しゅくまる/tableViewList.swift
@@ -12,6 +12,18 @@ class tableViewList: UIViewController,UITableViewDataSource, UITableViewDelegate
     
     var imgArray = Array<String>()
     var label2Array = Array<String>()
+    var buttonArray = Array<Int>()
+    var img2Array = Array<Int>()
+    
+    @IBOutlet weak var TodayDate: UILabel!
+    
+    @IBOutlet weak var shukudaisuu2: UILabel!
+    
+    @IBOutlet weak var shukudaisuu: UILabel!
+    
+    @IBOutlet weak var finish: UILabel!
+    
+    @IBOutlet weak var finish2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,7 +67,26 @@ class tableViewList: UIViewController,UITableViewDataSource, UITableViewDelegate
                 break
             }
         }
+        
+        buttonArray = [Int](repeating: 0, count: label2Array.count)
+        img2Array = [Int](repeating: 0, count: label2Array.count)
+        
+        var shukudaiCount:Int = 0//宿題数を計算する値
+        var finishCount:Int = 0 //終わった数を計算する値
+            
+            shukudaiCount = label2Array.count
+            
+            for num in 0 ..< label2Array.count {
+                finishCount += buttonArray[num]
+            }
+            shukudaisuu.text = String(shukudaiCount)
+            shukudaisuu2.text = String(shukudaiCount)
+            
+            finish.text = String(finishCount)
+
+
     }
+    
     
     //TableViewセルの数を指定
     func tableView(_ table: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -77,14 +108,30 @@ class tableViewList: UIViewController,UITableViewDataSource, UITableViewDelegate
         label1.text = String(describing: label2Array[indexPath.row])
         
         //Tag番号3でUIButtonインスタンスの生成
-        _ = cell.viewWithTag(3) as! UIButton
+        let button1 = cell.viewWithTag(3) as! UIButton
+        if(buttonArray[indexPath.row] == 0) {
+            button1.setTitle("これから!", for: .normal)
+            button1.setTitleColor(UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+            button1.backgroundColor = UIColor(red: 48/255, green: 148/255, blue: 137/255, alpha: 1.0)
+        }else if(buttonArray[indexPath.row] == 1) {
+            button1.setTitle("おわった!", for: .normal)
+            button1.setTitleColor(UIColor(red: 48/255, green: 148/255, blue: 137/255, alpha: 1.0), for: .normal)
+            button1.backgroundColor = UIColor.clear
+        }
         
         //Tag番号4でUIImageViewインスタンスの生成
-        _ = cell.viewWithTag(4) as! UIImageView
+        let imageView2 = cell.viewWithTag(4) as! UIImageView
+        if img2Array[indexPath.row] == 0 {
+            imageView2.image = UIImage(named: "はなまるoff")
+        }else if img2Array[indexPath.row] == 1 {
+            imageView2.image = UIImage(named: "はなまる")
+        }
+        
         
         return cell
         
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
